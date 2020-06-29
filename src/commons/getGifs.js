24fa43db665c963822fda54a45eec5df70762309
jeissonmgz <jeissonmgz@gmail.com>
2020-06-29 15:46:00 -0500

    JM Load categories in grid effect

---
+++ src/commons/getGifs.js
@@ -0,0 +1,15 @@
+export const getGifts = async (category) => {
+  const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(
+    category
+  )}&limit=10&api_key=ZefmqMjz8uhy3xSYf0SYps5gjRRYj9Om`;
+  const resp = await fetch(url);
+  const { data } = await resp.json();
+  const gifs = data.map((img) => {
+    return {
+      id: img.id,
+      title: img.title,
+      url: img.images?.downsized_medium.url,
+    };
+  });
+  return gifs;
+};
--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,24 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { GiftGridItem } from "./GiftGridItem";
+import { getGifts } from "../commons/getGifs";
 
 export const GifGrid = ({ category }) => {
   const [images, setImages] = useState([]);
   useEffect(() => {
-    getGifts(category);
-  }, []);
-  const getGifts = async (category) => {
-    const url = `https://api.giphy.com/v1/gifs/search?q=${category}&limit=10&api_key=ZefmqMjz8uhy3xSYf0SYps5gjRRYj9Om`;
-    const resp = await fetch(url);
-    const { data } = await resp.json();
-    const gifs = data.map((img) => {
-      return {
-        id: img.id,
-        title: img.title,
-        url: img.images?.downsized_medium.url,
-      };
-    });
-    setImages(gifs);
-  };
+    getGifts(category).then((gifs) => setImages(gifs));
+  }, [category]);
 
   return (
     <>
