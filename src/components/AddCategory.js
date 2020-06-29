d7d024bd111dec1c461d5851a016549b86550b36
jeissonmgz <jeissonmgz@gmail.com>
2020-06-29 14:40:03 -0500

    JM Add Category component

---
+++ src/components/AddCategory.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+const AddCategory = ({ setCategories }) => {
+  const [inputValue, setInputValue] = useState("");
+  const handleInputChange = ({ target: { value } }) => setInputValue(value);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (inputValue.trim().length > 2) {
+      setCategories((categories) => [...categories, inputValue]);
+      setInputValue("");
+    }
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={inputValue} onChange={handleInputChange} />
+    </form>
+  );
+};
+
+AddCategory.propTypes = {
+  setCategories: PropTypes.func.isRequired,
+};
+export { AddCategory };
