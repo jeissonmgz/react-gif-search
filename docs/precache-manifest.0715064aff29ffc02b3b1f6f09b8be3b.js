4c4d9b25ccb31bfa03864b35569519788422a478
jeissonmgz <jeissonmgz@gmail.com>
2020-06-29 16:32:44 -0500

    JM Docs to deploy in github

---
+++ docs/precache-manifest.0715064aff29ffc02b3b1f6f09b8be3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6ed3ce325fb9b4101a1b9e985d793b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b3224583125284362a5",
+    "url": "/static/css/main.7b793d76.chunk.css"
+  },
+  {
+    "revision": "838c7036f9a1a1889fe2",
+    "url": "/static/js/2.c91a6c4f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c91a6c4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b3224583125284362a5",
+    "url": "/static/js/main.08acf278.chunk.js"
+  },
+  {
+    "revision": "0e5545f79d18f708cb72",
+    "url": "/static/js/runtime-main.9f6029f6.js"
+  }
+]);
